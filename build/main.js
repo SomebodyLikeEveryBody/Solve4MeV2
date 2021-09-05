8a781c96b1968c69013795616d83c0040fe1f30c
Victor Vague <48601663+SomebodyLikeEveryBody@users.noreply.github.com>
2021-09-05 11:13:51 +0200

    Delete build directory

--- build/main.js
+++
@@ -1,5 +0,0 @@
-const saverNOpenerStateManager = new SaverNOpenerStateManager();
-const firstMathLineInput = new MathLineInput($('#input_screen'), saverNOpenerStateManager);
-
-firstMathLineInput.appendToContainer().focus();
-
